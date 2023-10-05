1052897e56a579b9bd499006eb39f1dd8b8dd22c
shamshath begum <sham29.b@gmail.com>
2023-10-05 10:41:32 +0530

    render link added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import Homepage from "./Pages/Homepage";
 import './App.css';
 import { Route ,Routes} from "react-router-dom";
 import Chatpage from "./Pages/Chatpage";
-export const url="http://localhost:5000"
+// export const url="http://localhost:5000"
+export const url="https://chat-app-be-88dx.onrender.com"
+
 
 function App() {
   return (
